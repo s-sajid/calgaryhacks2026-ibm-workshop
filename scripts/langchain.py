65bd45cd303d53334e8aed2ffa06da7fc12625de
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2026-02-14 02:02:29 -0700

    Add openai langchain

--- scripts/langchain.py
+++ scripts/langchain.py
@@ -1,15 +1,12 @@
 import os
 from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-"""
-Set to "cloud" to use Ollama Cloud
-or
-Set to "local" to use Ollama locally
-"""
-BACKEND = "local"
+# "ollama_local" or "ollama_cloud" or "openai"
+BACKEND = "ollama_local"
 
 user_prompt = "What model are you?"
 
@@ -17,7 +14,7 @@ messages = [
     {"role": "user", "content": user_prompt},
 ]
 
-if BACKEND == "cloud":
+if BACKEND == "ollama_cloud":
     MODEL = "gemini-3-flash-preview"
     llm = ChatOllama(
         model=MODEL,
@@ -26,11 +23,17 @@ if BACKEND == "cloud":
             "headers": {"Authorization": f"Bearer {os.getenv('OLLAMA_API_KEY')}"}
         },
     )
-elif BACKEND == "local":
+elif BACKEND == "ollama_local":
     MODEL = "granite4:350m"
     llm = ChatOllama(model=MODEL, base_url="http://localhost:11434")
+elif BACKEND == "openai":
+    MODEL = "gpt-5-mini"
+    llm = ChatOpenAI(
+        model=MODEL,
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
 else:
-    raise ValueError("BACKEND must be 'cloud' or 'local'.")
+    raise ValueError("BACKEND must be 'ollama_cloud', 'ollama_local', or 'openai'.")
 
 print(f"Backend: {BACKEND} | Model: {MODEL}")
 
